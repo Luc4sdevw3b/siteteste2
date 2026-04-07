6e499bcb04bd8c88eb605244c8dc71e8e6456232
Lucas <lucasleoques@gmail.com>
2026-04-07 11:14:42 -0300

    adicionado a pasta public com o jooj e a mudança no index para ele aparecer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Image from 'next/image'
 function Home( ) {
-    return <h1>Faz o L</h1>
+    return (
+    <>
+    <h1>Faz o L</h1>
+    <Image
+    src="/jooj.gif"
+    alt="Pássaro dançando"
+    width={480}
+    height={356}
+    />
+    </>
+    )
 }
 
 export default Home;
